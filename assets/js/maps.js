7b3cfec351022a976fa9a6fbb8137e0f245ed457
ConorM2710 <conormoyles@outlook.com>
2023-01-11 17:34:22 +0000

    finished resume

---
+++ assets/js/maps.js
@@ -0,0 +1,27 @@
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 46.619261,
+                    lng: -33.134766
+                }
+            });
+
+            var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var locations = [
+                { lat: 40.785091, lng: -73.968285},
+                { lat: 41.084045, lng: -73.874245},
+                { lat: 40.754932, lng: -73.984016}
+            ];
+
+            var markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: label[i % labels.length]
+                });
+            });
+
+            //marker from google var marker copy and paste here
+        }
